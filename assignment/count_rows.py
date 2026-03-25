29f3f8f4c2761f9e68eacc4af32558aa056cbe27
vivek18mhaske <mhaske.vivek18@gmail.com>
2026-03-25 10:12:40 +0530

    Refactor row counting in count_rows.py

--- assignment/count_rows.py
+++ assignment/count_rows.py
@@ -1,23 +1,10 @@
-# import csv
-
-# count = 0
-
-# with open('data.csv', 'r') as file:
-#     reader = csv.reader(file)
-    
-#     for row in reader:
-#         count += 1
-
-# print("Total number of rows:", count)
-
 import csv
 
-# Open the CSV file
 file_name = "data.csv"
 with open(file_name, 'r') as file:
  reader = csv.reader(file)
  row_count = 0
-# Count rows
+
  for row in reader:
   row_count += 1
 print("Total number of rows in the CSV file:", row_count)
