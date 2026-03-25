e0745f0589dc6739ea9c7268df9a1f66e51fed22
vivek18mhaske <mhaske.vivek18@gmail.com>
2026-03-25 10:09:28 +0530

    Add files via upload

--- json_to_csv/json_to_csv.py
+++ json_to_csv/json_to_csv.py
@@ -1,1 +1,15 @@
+import json
+import csv
 
+
+with open('data.json', 'r') as json_file:
+    data = json.load(json_file)
+
+with open('data.csv', 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+
+    writer.writerow(data[0].keys())
+    for item in data:
+        writer.writerow(item.values())
+
+print("JSON data successfully converted to CSV")
